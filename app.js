cee21e6712b2903fc2091abcb708cc9291ff7b17
dee-res <restrepo.diana.pat@gmail.com>
2024-10-15 19:19:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 'use strict';
   
 angular.module('lunchCheck', [])
-.controller('LunchCheckController', ['$scope, '$filter', LunchCheckController]);
+.controller('LunchCheckController', LunchCheckController);
 
+  LunchCheckController.$inject = ['$scope', '$filter'];
   function LunchCheckController($scope, $filter) {
     scope.name = "lunch";
 
