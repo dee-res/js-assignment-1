96b91409bf1084abca39859e6b0f57bc0a5e81c1
dee-res <restrepo.diana.pat@gmail.com>
2024-10-16 21:44:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 (function () {
 'use strict';
-  angular.module('LCApp', [])
-  .LCController('myFilter', function() {
-    return function(input) {
-      return 'Prefix: ' + input;
+  angular.module('LunchCheck', [])
+  .controller('LCController', LCController);
+
+  LCController.$inject = ['$scope'];
+
+  function LCController($scope) {
+    $scope.dishes = '';
+    $scope.message = '';
+    $scope.checked = false;
+
+    $scope.checkLunch = function() {
+      if ($scope.dishes.trim().length === 0) {
+        $scope.empty = true;
+      } else {
+          $scope.checked = true;
+          $scope.empty = false:
+          var arrayDishes = $scope.dishes.split(,);
+          var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
+            return v.trim() !== '';
+          });
+
+          if (arrayDishesWithoutEmptys.length <=3) {
+            $scope.message = 'Enjoy!';
+          } else {
+              $scope.message = 'Too much!';
+          }
+      }
     };
-  });
+  }
+})();
+            
+          
+
 
   
   
