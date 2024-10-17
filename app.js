d2c5b735b2c98587ea42f8db36fc4de6faf05843
dee-res <restrepo.diana.pat@gmail.com>
2024-10-16 23:16:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function() {
-    angular.module('LunchCheck', [])
+    angular.module('lunchCheck', [])
     .controller('LunchCheckController', LunchCheckController);
   
     LunchCheckController.$inject = ['$scope'];
@@ -25,44 +25,6 @@
             
           
 
-
-  
-  
-  //angular.module('LCApp', [])
-//.controller('LCController', LCController);
-
-//LCController.$inject = ['$scope'];
-//function LCController($scope) {
-//$scope.name= "lunch check";
-
-//  $scope.sayMessage = function () {
-//    return "Enjoy!";
-//  };
-//}
-// })();
-
-//LCController.$inject = ['$scope'];
-// function LCController($scope) {
- //  $scope.name= "lunch check";
-
-//   $scope.sayMessage = function () {
- //    return "Enjoy!"
- // };
-// }
-//})();
-
-
-
-//const str = 'Enjoy!'
-
-//const words = str.split(' ');
-//console.log(words[3]);
-
-//const chars = str.split(' ');
-//console.log(chars[8];
-
-//const strCopy = str.split();
-//console.log(strCopy);
 
   
   
